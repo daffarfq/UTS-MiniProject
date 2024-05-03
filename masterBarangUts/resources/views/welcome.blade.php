9e861bd7b9b4a03826f692720d2a197df50296d6
daffarfq <168827362+daffarfq@users.noreply.github.com>
2024-05-03 18:40:22 +0700

    Add files via upload

---
+++ masterBarangUts/resources/views/welcome.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Welcome</title>
+    @vite('resources/sass/app.scss')
+</head>
+<body>
+  <div class="container text-center my-5">
+    <h1 class="text-primary mb-4 display-3 fw-bold">Selamat Datang</h1>
+    <div class="card bg-info text-white shadow-lg">
+      <div class="card-body">
+        <p class="lead mb-4"><strong>Informasi Mahasiswa:</strong></p>
+        <ul class="list-unstyled">
+          <li><strong>Nama:</strong> Muhammad Daffa Rifqi</li>
+          <li><strong>Kelas:</strong> SI 05-03</li>
+          <li><strong>NIM:</strong> 1204220011</li>
+          <li><strong>Prodi:</strong> Sistem Informasi</li>
+          <li><strong>Kampus:</strong> University Telkom Surabaya</li>
+        </ul>
+      </div>
+    </div>
+    <a class="btn btn-outline-primary mt-4" href="{{ route('home') }}">Kembali ke Beranda</a>
+  </div>  
+    @vite('resources/js/app.js')
+</body>
+</html>
